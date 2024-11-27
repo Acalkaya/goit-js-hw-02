f9e7ad6ab767ed43cf5df7de8d5db2a767773da4
Aysun Çalkaya <aysuncalkaya@gmail.com>
2024-11-27 22:58:54 +0300

    update

--- js/task-3.js
+++ js/task-3.js
@@ -8,7 +8,6 @@ function checkForSpam(message) {
   
     return false;
   }
-  
   console.log(checkForSpam("Latest technology news")); // false
   console.log(checkForSpam("JavaScript weekly newsletter")); // false
   console.log(checkForSpam("Get best sale offers now!")); // true
